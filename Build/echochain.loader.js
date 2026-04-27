bfc7b8562253b041ec1cfd0fb4f3699883f7677a
Abby <34819442+SirePherox@users.noreply.github.com>
2026-04-27 16:05:24 +0100

    Updates

--- Build/echochain.loader.js
+++ Build/echochain.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 76714017,
+    wasmFileSize: 76713860,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
